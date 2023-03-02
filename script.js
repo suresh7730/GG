6ac036da2ebc26cc4524f86c464bc41120e70ba4
Sureshkumar <suresh7730@yahoo.com>
2023-03-02 20:09:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
-/********************************************************************
- * Demo created by Jason Mayes 2020.
- *
- * Got questions? Reach out to me on social:
- * Twitter: @jason_mayes
- * LinkedIn: https://www.linkedin.com/in/creativetech
- ********************************************************************/
-
 const demosSection = document.getElementById('demos');
 
 var model = undefined;
@@ -58,6 +33,16 @@ cocoSsd.load().then(function (loadedModel) {
   // Show demo section now model is ready to use.
   demosSection.classList.remove('invisible');
 });
+
+
+async function load_model() {
+    const model = await loadGraphModel('http://192.168.1.35:1883/getmodel/model.json');
+    return model;
+  }
+
+load_model();
+demosSection.classList.remove('invisible');
+
 /********************************************************************
 // Demo 1: Grab a bunch of images from the page and classify them
 // upon click.
