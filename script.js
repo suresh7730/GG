72d1e8da2e54b61b8f4bce68aec1ef7df4681aee
Sureshkumar <suresh7730@yahoo.com>
2023-03-03 15:09:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@
 
   }
   
-  alert(classesDir[1].name,classesDir[2].name);
+  alert(classesDir[1].name,classesDir[2].name,'abc');
 
   const modelPromise = load_model();
   alert('at modelPromise');
